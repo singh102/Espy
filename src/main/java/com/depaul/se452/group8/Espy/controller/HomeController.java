8204fe0e1b21fd8aa0b22788df2c7ba811d7a912
Brandon W <brondo765@gmail.com>
2021-04-17 16:03:31 -0500

    Added basic classes for persistence later on
    Worked with Karthik earlier today and decided on the basic infrastructure of our project. Added essentially, getters and setters for model classes.

--- src/main/java/com/depaul/se452/group8/Espy/controller/HomeController.java
+++ src/main/java/com/depaul/se452/group8/Espy/controller/HomeController.java
@@ -1,10 +1,10 @@
-package com.depaul.se452.group8.Espy;
+package com.depaul.se452.group8.Espy.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Home {
+public class HomeController {
 
     @RequestMapping("/")
     public String home() {
