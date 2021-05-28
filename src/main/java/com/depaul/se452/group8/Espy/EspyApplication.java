22ebedb9c87d6aa1685be37395fd7d0d84c13e3d
Karthik.Singh <Karthik.Singh73@Gmail.com>
2021-05-27 22:04:40 -0500

    adding security config update to unblock, updating navbar

--- src/main/java/com/depaul/se452/group8/Espy/EspyApplication.java
+++ src/main/java/com/depaul/se452/group8/Espy/EspyApplication.java
@@ -2,8 +2,9 @@ package com.depaul.se452.group8.Espy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 public class EspyApplication {
 
 	public static void main(String[] args) {
