ecda16621f8e4413a39b6e9380eb18f8fb637e46
Karthik.Singh <Karthik.Singh73@Gmail.com>
2021-05-31 09:32:06 -0500

    friends detail page

--- src/main/java/com/depaul/se452/group8/Espy/controller/FriendsController.java
+++ src/main/java/com/depaul/se452/group8/Espy/controller/FriendsController.java
@@ -2,12 +2,18 @@ package com.depaul.se452.group8.Espy.controller;
 
 import com.depaul.se452.group8.Espy.model.Blocked;
 import com.depaul.se452.group8.Espy.model.Friends;
+import com.depaul.se452.group8.Espy.model.User;
 import com.depaul.se452.group8.Espy.repository.BlockedRepository;
 import com.depaul.se452.group8.Espy.repository.FriendsRepository;
+import com.depaul.se452.group8.Espy.service.ImageService;
+import com.depaul.se452.group8.Espy.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
 import java.time.LocalDateTime;
+import java.util.Optional;
 import javax.validation.Valid;
 
 @Controller
@@ -19,51 +25,22 @@ public class FriendsController extends BaseController {
     @Autowired
     private BlockedRepository blockedRepository;
 
-    @GetMapping(value = "/friends")
-    public String friends() {
-        // Friends Demonstration
-        Friends friend = new Friends();
-        friend.setUserId(1);
-        friend.setFriendId(2);
-        friend.setUpdatedAt(LocalDateTime.now());
-        friend.setCreatedAt(LocalDateTime.now());
-        friendsRepository.save(friend);
+    @Autowired
+    private UserService userService;
 
-        // Blocked Users Demonstration
-        Blocked block = new Blocked();
-        block.setUserId(1);
-        block.setFriendId(2);
-        block.setUpdatedAt(LocalDateTime.now());
-        block.setCreatedAt(LocalDateTime.now());
-        blockedRepository.save(block);
+    @Autowired
+    private ImageService imageService;
 
-        return "<h1>Friends Page!</h1>" + "\r\n" +
-                "<h3>Friends:</h3>" + friendsRepository.findAll() + "\r\n" +
-                "<h3>Blocked Users:</h3>" + blockedRepository.findAll();
-    }
+    @GetMapping(value = "/friend/{id}")
+    public ModelAndView friends(@PathVariable(value = "id")Integer id) {
+        ModelAndView viewModel = new ModelAndView("friends");
 
-    @GetMapping("/friends/{id}")
-    public Friends getFriendById(@PathVariable Integer id) {
-        return friendsRepository.findById(id).get();
-    }
+        Optional<Friends> friend = friendsRepository.findById(id);
 
-    @PostMapping("/friends")
-    public Friends saveFriend(@Valid Friends friend) {
-        return friendsRepository.save(friend);
-    }
+        User user = userService.getUserById(friend.get().getUserId());
 
-    @DeleteMapping("/friends/{id}")
-    public void deleteFriend(@PathVariable Integer id) {
-        friendsRepository.deleteById(id);
-    }
-
-    @PostMapping("/friends/block")
-    public Blocked block(@Valid Blocked blocked) {
-        return blockedRepository.save(blocked);
-    }
-
-    @DeleteMapping("/friends/block/{id}")
-    public void unblock(@PathVariable Integer id) {
-        blockedRepository.deleteById(id);
+        viewModel.addObject("user", user);
+        viewModel.addObject("posts", imageService.getAllImagesByUserId(user.getId()));
+        return viewModel;
     }
 }
