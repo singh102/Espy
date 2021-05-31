602e2c9cef4342e5a61d58404a74aceb415bb3e5
David Engel <davidengel@live.com>
2021-05-31 11:58:49 -0700

    Add search to home page

--- src/main/java/com/depaul/se452/group8/Espy/repository/UserRepository.java
+++ src/main/java/com/depaul/se452/group8/Espy/repository/UserRepository.java
@@ -2,10 +2,17 @@ package com.depaul.se452.group8.Espy.repository;
 
 import com.depaul.se452.group8.Espy.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     User getUserByUsername(String username);
+
+//    @Query(nativeQuery = true, value = "SELECT * FROM users WHERE LOWER(username) LIKE LOWER(?2) ORDER BY username ASC")
+    List<User> findAllByUsernameContaining(String query);
 }
 
