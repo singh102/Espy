2287969cc7e11537a8391f9c5312f8049ffbcfa7
Karthik.Singh <Karthik.Singh73@Gmail.com>
2021-05-31 15:15:57 -0500

    tags logic on upload

--- src/main/java/com/depaul/se452/group8/Espy/controller/ProfileController.java
+++ src/main/java/com/depaul/se452/group8/Espy/controller/ProfileController.java
@@ -1,7 +1,10 @@
 package com.depaul.se452.group8.Espy.controller;
 
+import com.depaul.se452.group8.Espy.model.Images;
+import com.depaul.se452.group8.Espy.model.Tags;
 import com.depaul.se452.group8.Espy.model.User;
 import com.depaul.se452.group8.Espy.repository.FavoritesRepository;
+import com.depaul.se452.group8.Espy.repository.TagsRepository;
 import com.depaul.se452.group8.Espy.service.UserDetailsImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -10,11 +13,17 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class ProfileController extends BaseController {
     @Autowired
     FavoritesRepository favoritesRepository;
+
+    @Autowired
+    TagsRepository tagsRepository;
 
     @GetMapping("/profile")
     public ModelAndView profile() {
@@ -43,8 +52,24 @@ public class ProfileController extends BaseController {
     @PostMapping("/addImageComment")
     public String addImageComment(@RequestParam("id")Integer id,
                                   @RequestParam("imagePost")MultipartFile imageFile,
-                                  @RequestParam("caption")String caption) throws IOException {
-        imageService.addImageAndCaptionToUser(id, imageFile, caption);
+                                  @RequestParam("caption")String caption,
+                                  @RequestParam("tags")String tags) throws IOException {
+
+        Images image = imageService.addImageAndCaptionToUser(id, imageFile, caption);
+
+        List<String> tagList = Arrays.asList(tags.split(","));
+
+        if (image != null && tagList != null && tagList.size() > 0) {
+            for (String tag : tagList) {
+                Tags tagModel = new Tags();
+                tagModel.setTag(tag);
+                tagModel.setImageId(image.getId());
+                tagModel.setCreatedAt(LocalDateTime.now());
+                tagModel.setUpdatedAt(LocalDateTime.now());
+                tagsRepository.save(tagModel);
+            }
+        }
+
         return "redirect:/profile";
     }
 }
